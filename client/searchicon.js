3a44fa57030ddfc180391d5bc2b7ac5e27864111
bavgg <gestopajonas@gmail.com>
2024-06-26 10:29:12 +0800

    add: homepage

---
+++ client/searchicon.js
@@ -0,0 +1,3 @@
+var searchicon = `<svg fill="#000000" width="34px" height="34px" viewBox="0 0 24 24" id="search" data-name="Line Color" xmlns="http://www.w3.org/2000/svg" class="icon line-color"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><line id="secondary" x1="21" y1="21" x2="15" y2="15" style="fill: none; stroke: #2ca9bc; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></line><circle id="primary" cx="10" cy="10" r="7" style="fill: none; stroke: #000000; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></circle></g></svg>`;
+
+document.write(searchicon);
