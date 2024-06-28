7ed91ae985747f8e3b03f47936f3b53d264c7e4a
bavgg <gestopajonas@gmail.com>
2024-06-28 08:25:22 +0800

    update: logout ui

--- client/index.mjs
+++ client/index.mjs
@@ -5,14 +5,25 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "/login";
   }
   const token = getDecodedAuthToken();
-  if(token !== undefined) {
-    (document.getElementById('fullname')).textContent = token.firstname + ' ' + token.lastname;
+  if (token !== undefined) {
+    document.getElementById("fullname").textContent =
+      token.firstname + " " + token.lastname;
   }
 
-  const logoutButton = document.getElementById('c121b');
+  const logoutButton = document.getElementById("c121b");
 
-  logoutButton.addEventListener('click', () => {
-    localStorage.removeItem('authToken');
+  logoutButton.addEventListener("click", () => {
+    localStorage.removeItem("authToken");
     window.location.href = "/";
+  });
+
+  const profileButton = document.getElementById("c122");
+  const logoutContainer = document.getElementById("c121");
+  profileButton.addEventListener("click", () => {
+    if (logoutContainer.style.display === "none") {
+      logoutContainer.style.display = "flex";
+    } else {
+      logoutContainer.style.display = "none";
+    } 
   });
 });
